05b2c8b6099b56af34769e406a682c27d68c672c
Till Dettmering <prinzipiell@gmail.com>
2013-04-06 12:16:43 +0200

    Works! Datasets with different means, sds and ns are now aggregated. After http://books.google.de/books?id=cn3JtP5ugQsC&lpg=PA141&ots=YHV9PQJB9v&dq=varianz%20von%20%20zusammengefasste%20gruppen%20formel&hl=de&pg=PA141#v=onepage&q&f=false

--- aggregated_datasets.py
+++ aggregated_datasets.py
@@ -1,12 +1,56 @@
 #!/usr/bin/python
+# based on http://books.google.de/books?id=cn3JtP5ugQsC&lpg=PA141&ots=YHV9PQJB9v&dq=varianz%20von%20%20zusammengefasste%20gruppen%20formel&hl=de&pg=PA141#v=onepage&q&f=false
 
 import sys
-import math
 import numpy as np
-import scipy as np
 import dataimporter as di
 
 filename = sys.argv[1]
 data = di.readCsv(filename, ' ')
 
 print data
+
+# 	Input: List in format: Mean SD NumberOfMeasurements
+
+def aggregate(data):
+	k = len(data)
+	
+#	Define variables needed for formula
+
+	x_array = []
+	n_array = []
+	
+	for i in data:
+		x_array.append(float(i[0]))
+		n_array.append(int(i[2]))
+
+	x_bar = np.mean(x_array)
+	n = float(np.sum(n_array))
+	
+#	Perform calculation
+#	First summand s1
+
+	s1_array = []
+
+	for s1 in data:
+		result1 = float(s1[2]) / n * float(s1[1]) * float(s1[1])
+		s1_array.append(result1)
+		
+	s1_sum = np.sum(s1_array)
+
+#	Second summand s2
+
+	s2_array = []
+	
+	for s2 in data:
+		result2 = float(s2[2]) / n * (float(s2[0]) - x_bar) * (float(s2[0]) - x_bar)
+		s2_array.append(result2)
+		
+	s2_sum = np.sum(s2_array)
+	
+	var = s1_sum + s2_sum
+	sd = np.sqrt(var)
+	
+	print x_bar, sd, n
+		
+aggregate(data)
