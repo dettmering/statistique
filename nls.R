15a0d7a1a4c7df78f0ac668b20c3d112c9bcf9e9
Till Dettmering <prinzipiell@gmail.com>
2013-05-02 17:52:57 +0200

    Fixed weight bug

--- nls.R
+++ nls.R
@@ -2,11 +2,10 @@
 # Model: f(x) = 1 / (1 + exp(-k*(x-d50)))
 
 fitDoseresponse <- function(raw) {
-	data <- data.frame(raw[,1],raw[,2])
-	weight <- raw[,3]
-	colnames(data) <- c("X","Y")
+	data <- data.frame(raw[,1],raw[,2],raw[,3])
+	colnames(data) <- c("X","Y","N")
 	
-	model <- nls("Y ~ 1/(1+exp(-k*(X - d50)))", data=data, start=list(k=2,d50=11), weigths=weight)
+	model <- nls("Y ~ 1/(1+exp(-k*(X - d50)))", data=data, start=list(k=2,d50=11), weigths=data[,"N"])
 	
 	ck <- summary(model)$coefficients[1]
 	cd50 <- summary(model)$coefficients[2]
