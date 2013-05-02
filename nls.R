cfef1095ba3e2066aee7298e7a8aafa65a0d4e18
Till Dettmering <prinzipiell@gmail.com>
2013-05-02 14:40:32 +0200

    Fixed error in formula, now outputs graph.

--- nls.R
+++ nls.R
@@ -5,4 +5,10 @@ raw <- read.table("D:/test.dat")
 data <- data.frame(raw[,1],raw[,2])
 colnames(data) <- c("X","Y")
 
-nls("Y ~ 1/(exp(-k*(X - d50)))", data=data, start=list(k=1,d50=11))
+nls("Y ~ 1/(1+exp(-k*(X - d50)))", data=data, start=list(k=1,d50=11))
+
+ck <- summary(model)$coefficients[1]
+cd50 <- summary(model)$coefficients[2]
+
+plot(data[,"X"],data[,"Y"], type ="p", main = "Y vs. X", xlim=c(1,20), ylim=c(0,1))
+curve(1/(1+exp(-ck*(x-cd50))), add=TRUE)
