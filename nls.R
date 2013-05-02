d3b8953af59e07dbece90bbe1e9de45662e3eeb4
Till Dettmering <prinzipiell@gmail.com>
2013-05-02 21:42:01 +0200

    Adjusted columns to new output. Function now outputs Std. Error of variable

--- nls.R
+++ nls.R
@@ -2,16 +2,18 @@
 # Model: f(x) = 1 / (1 + exp(-k*(x-d50)))
 
 fitDoseresponse <- function(raw) {
-	data <- data.frame(raw[,1],raw[,2],raw[,3])
-	colnames(data) <- c("X","Y","N")
+	data <- data.frame(raw[,1],raw[,2],raw[,3],raw[,4])
+	colnames(data) <- c("X","Y","R","N")
 	
 	model <- nls("Y ~ 1/(1+exp(-k*(X - d50)))", data=data, start=list(k=2,d50=11), weigths=data[,"N"])
 	
 	ck <- summary(model)$coefficients[1]
 	cd50 <- summary(model)$coefficients[2]
+	ck_err <- summary(model)$coefficients[3]
+	cd50_err <- summary(model)$coefficients[4]
 	
 	plot(data[,"X"],data[,"Y"], type ="p", main = "Y vs. X", xlim=c(1,20), ylim=c(0,1))
 	curve(1/(1+exp(-ck*(x-cd50))), add=TRUE)
 
-	return(c(cd50, ck))
+	return(c(cd50, cd50_err, ck, ck_err))
 }
