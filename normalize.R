4ea784a3322e1aac5816b12da33e1dbac07d472c
Till Dettmering <prinzipiell@gmail.com>
2013-07-15 12:50:19 +0200

    Removed bug, normalizes to 1 now. Added function to normalize raw data.

--- normalize.R
+++ normalize.R
@@ -1,6 +1,13 @@
-normalizeData <- function(a1, a2, b1, b2) {
-	normalmean <- a1 / b1
-	normalsd <- normalmean * sqrt((b2/b1)^2 + (a2/a1)^2)
+normalizeData <- function(mean1, sd1, mean2, sd2) {
+	normalmean <- mean1 / mean2
+	normalsd <- sqrt((mean2/sd2)^2 + (mean1/sd1)^2)
 
 	return(cbind(normalmean, normalsd))
+}
+
+normalizeRaw <- function(data, ctrl) {
+  normalmean <- mean(data) / mean(ctrl)
+  normalsd <- sqrt((sd(data)/mean(data))^2 + (sd(ctrl)/mean(ctrl))^2)
+  
+  return(cbind(normalmean, normalsd))
 }
