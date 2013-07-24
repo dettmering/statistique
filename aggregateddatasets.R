a9aa3825c2dc42342d1e0584d034b6792af62fe8
Till Dettmering <prinzipiell@gmail.com>
2013-07-24 16:31:45 +0200

    Added unweighted sd and sem

--- aggregateddatasets.R
+++ aggregateddatasets.R
@@ -5,15 +5,17 @@ aggregateDataset <- function(a) {
 #	a <- read.table(thefile, comment.char = "#", header= FALSE, blank.lines.skip=TRUE)
 
 	x <- mean(a[,1])
+  sd <- sd(a[,1])
 	k <- length(a[,1])
 	n <- sum(a[,3])
 
 	variance <- sum(a[,3] / n * a[,2] * a[,2]) + sum(a[,3] / n * (a[,1] - x) * (a[,1] - x))
-	sd <- sqrt(variance)
+	sd_weight <- sqrt(variance)
+	sem_weight <- sd_weight / sqrt(n)
 	sem <- sd / sqrt(n)
 
 	x_weight <- sum(a[,3] / n * a[,1])
 
-	aggr <- cbind(x, x_weight, sd, sem, n, k)
+	aggr <- cbind(x, sd, sem, x_weight, sd_weight, sem_weight, n, k)
 	return(aggr)
 }
