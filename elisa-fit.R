f32f2b5b3f38c0858f918565170f57c98f022759
Till Dettmering <prinzipiell@gmail.com>
2013-10-17 09:24:13 +0200

    Better Workflow

--- elisa-fit.R
+++ elisa-fit.R
@@ -7,6 +7,8 @@ getConc <- function(OD, dilution, logmod.a, logmod.b, logmod.c, logmod.d) {
   return(result)
 }
 
+a <- subset(standardkurven, ELISA.No == 19)
+
 logmod <- drm(formula = a$OD ~ a$Conc, fct = LL.4())
 
 logmod.b <- logmod$coef[1]
@@ -14,11 +16,12 @@ logmod.c <- logmod$coef[2]
 logmod.d <- logmod$coef[3]
 logmod.e <- logmod$coef[4]
 
-plot(logmod)
+plot(logmod, main = unique(a$Designation))
 
+data <- read.table("clipboard")
 data$conc <- getConc(data$V1, dilution, logmod.a, logmod.b, logmod.c, logmod.d)
 points(data$V1 ~ data$conc, col = "red")
 data$conc.dilution <- data$conc * dilution
 # Subtract medium manually!
 
-write.csv(data, "~/Desktop/elisa.txt")
+write.table(data$final,"clipboard", row.names=F, col.names=F)
