340d470a566d908bd8979e4e2f879c31e83552d6
Till Dettmering <prinzipiell@gmail.com>
2013-10-17 11:00:45 +0200

    Readability of code

--- tsim.R
+++ tsim.R
@@ -9,26 +9,21 @@ treated.sd <- 0.22
 # Returns a list of p-values from the test
 
 tsim <- function(rpt, n, mean1, sd1, mean2, sd2) {
-  x <- 0
-  ppool <- NULL
-  while (x <= rpt) {
-    ppool <- c(ppool, t.test(rnorm(n,mean1,sd1), y = rnorm(n,mean2,sd2))$p.value)
-    x <- x + 1
-  }
-  return(ppool)
+  pvals <- NULL
+  pvals <- replicate(rpt, t.test(rnorm(n,mean1,sd1), y = rnorm(n,mean2,sd2))$p.value)
+
+  return(pvals)
 }
 
 # Iterate through sample sizes and run the tsim function
 # Returns data frame with list of mean p-values at a given sample size
 
-i <- 2
-num <- 50
+n_range <- 2:50
 res <- NULL
 
-while (i <= num) {
+for (i in n_range) {
   sim <- tsim(1000, i, ctrl.mean, ctrl.sd, treated.mean, treated.sd)
   res <- rbind(res, cbind(i, mean(sim), sd(sim)))
-  i <- i + 1
 }
 
 res <- as.data.frame(res)
@@ -38,11 +33,11 @@ res <- as.data.frame(res)
 ggplot(res, aes(x=i, y=-log10(V2))) +
   geom_line() +
   geom_ribbon(aes(ymin=-log10(V2)-log10(V3), ymax=-log10(V2)+log10(V3)), alpha = 0.2) +
-  annotate("segment", x = 6, xend = num, y = -log10(0.05), yend = -log10(0.05), colour = "red", linetype = "dashed") +
+  annotate("segment", x = 6, xend = max(n_range), y = -log10(0.05), yend = -log10(0.05), colour = "red", linetype = "dashed") +
   annotate("text",  x = 0, y=-log10(0.05), label= "p = 0.05", hjust=0, size=3) +
-  annotate("segment", x = 6, xend = num, y = -log10(0.01), yend = -log10(0.01), colour = "red", linetype = "dashed") +
+  annotate("segment", x = 6, xend = max(n_range), y = -log10(0.01), yend = -log10(0.01), colour = "red", linetype = "dashed") +
   annotate("text",  x = 0, y=-log10(0.01), label= "p = 0.01", hjust=0, size=3) +
-  annotate("segment", x = 6, xend = num, y = -log10(0.001), yend = -log10(0.001), colour = "red", linetype = "dashed") +
+  annotate("segment", x = 6, xend = max(n_range), y = -log10(0.001), yend = -log10(0.001), colour = "red", linetype = "dashed") +
   annotate("text",  x = 0, y=-log10(0.001), label= "p = 0.001", hjust=0, size=3) +
   xlab("Number of replicates") +
   ylab("-log10(p-value)") +
