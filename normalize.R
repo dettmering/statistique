c5de339d000131216b923ec40e22768bb1f9614d
Till Dettmering <prinzipiell@gmail.com>
2013-11-05 13:18:31 +0100

    Fixed critical mistake: Multiplication with "normalsd" was not done

--- normalize.R
+++ normalize.R
@@ -1,13 +1,13 @@
 normalizeData <- function(mean1, sd1, mean2, sd2) {
 	normalmean <- mean1 / mean2
-	normalsd <- sqrt((mean2/sd2)^2 + (mean1/sd1)^2)
+	normalsd <- normalmean * sqrt((mean2/sd2)^2 + (mean1/sd1)^2)
 
 	return(cbind(normalmean, normalsd))
 }
 
 normalizeRaw <- function(data, ctrl) {
   normalmean <- mean(data) / mean(ctrl)
-  normalsd <- sqrt((sd(data)/mean(data))^2 + (sd(ctrl)/mean(ctrl))^2)
+  normalsd <- normalmean * sqrt((sd(data)/mean(data))^2 + (sd(ctrl)/mean(ctrl))^2)
   
   return(cbind(normalmean, normalsd))
 }
